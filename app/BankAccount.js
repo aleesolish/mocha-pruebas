43b288bad7e8416c7f14a7d0a4917ad36a000463
aleesolish <aleesolish@gmail.com>
2020-10-24 16:48:43 -0500

    bank acc

--- app/BankAccount.js
+++ app/BankAccount.js
@@ -1,4 +1,46 @@
-exports.current = () => {
-    let balance = 1000
-    return balance
+module.exports = class bankAcc{
+
+  
+  constructor(balance, history){
+        this.balance = balance
+        this.historial = []
+    }
+
+    getbalance(){
+        return this.balance
+    }
+
+    append(monies){
+        if (monies < 0){
+            return ("That's not possible")
+        }
+        else{
+            this.balance += monies
+            this.historial.push(monies)
+        }
+        return this.balance
+    }
+
+    substract(monies){
+      if (monies < 0){
+          return ("That's not possible")
+      }
+      else{
+          this.balance -= monies
+          this.historial.push(monies)
+      }
+      return this.balance
   }
+
+
+  merge(original, other){
+    original.historial.concat(other.historial)
+    return (original.balance + other.balance)
+
+  }
+
+  historial_file(){
+    return this.historial
+  }
+  
+}
--- test/bankAcc.js
+++ test/bankAcc.js
@@ -2,11 +2,53 @@ const assert = require('assert');
 const BankAccount = require('../app/BankAccount')
 
 describe('BankAccount', () => {
-    describe('#current', () => {
+
+    describe('#getbalance', () => {
         it('Should return the balance', () => {
-         assert.strictEqual(BankAccount.current(), 1000);
+          let account = new BankAccount(0)
+          account.append(100)
+          assert.equal(100, account.getbalance())
         })
     })
 
+    describe('#getbalance', () => {
+        it('Should return the balance', () => {
+          let account = new BankAccount(0)
+          account.append(100)
+          account.substract(50)
+          assert.equal(50, account.getbalance())
+        })
+    })
+
+    describe('#merge', () =>{
+        it ('Should merge', () =>{
+            let original = new BankAccount(100)
+            let other = new BankAccount(200)
+            assert.strictEqual(300, original.merge(original,other))
+        })
+    })
+
+    describe('#getMergeHistorial', () =>{
+        it ('Should get the history merged', () =>{
+            let original = new BankAccount(100)
+            let other = new BankAccount(200)
+            original.append (100)
+            other.substract(30)
+            assert.strictEqual(370, original.merge(original,other))
+            assert.strictEqual([100, 30], original.historial_file() )
+        })
+    })
+
+    describe('#historial', () => {
+        it('Should be the history', () => {
+          let account = new BankAccount(0)
+          account.append(100)
+          account.substract(50)
+          assert.deepEqual([100, 50], account.historial_file() )
+        })
+    })
+    
+
 })
+
 
